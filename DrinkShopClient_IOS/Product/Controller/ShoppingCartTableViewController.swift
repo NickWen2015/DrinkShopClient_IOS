2fd814631fe8151d5feeeec7a79c7e5406b410e5
Mrosstro <ttosam01@gmail.com>
2018-12-06 23:25:34 +0800

    組長 修改 ShoppingCartTableViewController

--- DrinkShopClient_IOS/Product/Controller/ShoppingCartTableViewController.swift
+++ DrinkShopClient_IOS/Product/Controller/ShoppingCartTableViewController.swift
@@ -9,82 +9,44 @@
 import UIKit
 
 class ShoppingCartTableViewController: UITableViewController {
-
+    
+    var shoppingCartList: [ShoppingCart]!
+    
+    static var ShoppingCartVC: ShoppingCartViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        ShoppingCartViewController.ShoppingCartTableVC = self
     }
-
+    
+    private func getShoppingCardList() -> [ShoppingCart] {
+        let logSQLite = LogSQLite.init()  // 資料庫
+        return logSQLite.searchAllProductInShoppingCart()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //        ShoppingCartTableViewController.ShoppingCartVC = nil
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        shoppingCartList = getShoppingCardList()
+        return shoppingCartList.count
     }
-
-    /*
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingCartList", for: indexPath) as! ShoppingCartListTableViewCell
+        cell.shoppingCarContent = shoppingCartList[indexPath.row]
+        cell.tableView = tableView
+        cell.shoppingCartViewController = ShoppingCartTableViewController.ShoppingCartVC
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
